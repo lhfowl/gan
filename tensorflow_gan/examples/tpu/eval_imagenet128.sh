6700fe7be486f8b0a16e3fce434e064762f7723e
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-03 19:45:31 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_imagenet128.sh
+++ tensorflow_gan/examples/tpu/eval_imagenet128.sh
@@ -1,14 +1,14 @@
 
-export EXPERIMENT_NAME=imagenet64_fixed
+export EXPERIMENT_NAME=imagenet128_baseline
 export BATCH_SIZE=1024
 export TRAIN_STEPS_PER_EVAL=10000
-export DATASET_ARGS='--image_size=64 \
---dataset_name=imagenet_resized/64x64 \
+export DATASET_ARGS='--image_size=128 \
+--dataset_name=imagenet2012 \
 --num_classes=1000 \
 --dataset_val_split_name=validation'
 export ADDITIONAL='--critic_type=acgan \
 --num_eval_steps=1 \
 --mode=intra_fid_eval'
 
-
 bash tpu/_eval_base.sh
+
