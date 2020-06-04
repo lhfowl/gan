02cfd7249837f67ccf7df039f0e3660e93a272b0
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-04 15:52:09 -0400

    WIP

--- tensorflow_gan/examples/tpu/_base.sh
+++ tensorflow_gan/examples/tpu/_base.sh
@@ -5,7 +5,7 @@ python3.7 self_attention_estimator/train_experiment_main.py \
   --eval_on_tpu=true \
   --use_tpu_estimator=true \
   --mode=train_and_eval \
-  --max_number_of_steps=999999 \
+  --max_number_of_steps=9999999 \
   --train_batch_size=${BATCH_SIZE} \
   --eval_batch_size=${BATCH_SIZE} \
   --predict_batch_size=${BATCH_SIZE} \
