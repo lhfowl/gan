dcbb59193e5887680b6585ccdd251f64cb33a3b4
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-03 20:16:35 -0400

    WIP

--- tensorflow_gan/examples/gpu/eval_imagenet64_tyan.sh
+++ tensorflow_gan/examples/gpu/eval_imagenet64_tyan.sh
@@ -6,11 +6,11 @@ export DATA_DIR=/nfs/cluster/ilya/tfdf
 export BATCH_SIZE=128
 export DATASET_ARGS='--image_size=64 --dataset_name=imagenet_resized/64x64 --num_classes=1000 --dataset_val_split_name=validation'
 
-export ADDITIONAL='--critic_type=acgan \
+export ADDITIONAL="--critic_type=acgan \
 --num_eval_steps=8 \
 --mode=intra_fid_eval \
 --intra_fid_eval_start=${INTRA_FID_EVAL_START} \
---intra_fid_eval_end=${INTRA_FID_EVAL_END}'
+--intra_fid_eval_end=${INTRA_FID_EVAL_END}"
 
 # export ADDITIONAL='--critic_type=acgan \
 # --num_eval_steps=80 \
