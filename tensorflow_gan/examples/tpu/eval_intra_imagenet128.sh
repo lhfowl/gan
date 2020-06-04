67f069fbfd642979d7ba5bcdb7ecce99e4cd5694
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-04 19:33:33 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_intra_imagenet128.sh
+++ tensorflow_gan/examples/tpu/eval_intra_imagenet128.sh
@@ -7,8 +7,8 @@ export DATASET_ARGS='--image_size=128 \
 --num_classes=1000 \
 --dataset_val_split_name=validation'
 export ADDITIONAL='--critic_type=acgan \
---num_eval_steps=49 \
---mode=continuous_eval'
+--num_eval_steps=1 \
+--mode=intra_fid_eval'
 
 bash tpu/_eval_base.sh
 
