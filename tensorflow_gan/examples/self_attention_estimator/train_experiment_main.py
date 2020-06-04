3e2cee30f277842ae252bc9fae219bab8f629c00
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-03 22:24:51 -0400

    WIP

--- tensorflow_gan/examples/self_attention_estimator/train_experiment_main.py
+++ tensorflow_gan/examples/self_attention_estimator/train_experiment_main.py
@@ -29,7 +29,6 @@ from absl import app
 from absl import flags
 
 import logging
-import os
 
 flags.DEFINE_string('model_dir', '/tmp/tfgan_logdir/sagan-estimator',
                     'Optional location to save model. If `None`, use a '
@@ -156,7 +155,7 @@ def main(_):
   formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
   
   # create file handler
-  fh = logging.FileHandler(os.path.join(FLAGS.model_dir, 'tensorflow.log'))
+  fh = logging.FileHandler(FLAGS.model_dir + '/tensorflow.log')
   fh.setLevel(logging.INFO)
   fh.setFormatter(formatter)
   log.addHandler(fh)
