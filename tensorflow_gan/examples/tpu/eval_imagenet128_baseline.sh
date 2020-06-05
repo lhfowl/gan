8c69ea3e26cfdc244db2bdb8351c47af57aadfc7
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-05 06:35:36 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_imagenet128_baseline.sh
+++ tensorflow_gan/examples/tpu/eval_imagenet128_baseline.sh
@@ -1,5 +1,5 @@
 
-export EXPERIMENT_NAME=imagenet128_expt_2ctd
+export EXPERIMENT_NAME=imagenet128_baseline
 export BATCH_SIZE=1024
 export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=128 \
