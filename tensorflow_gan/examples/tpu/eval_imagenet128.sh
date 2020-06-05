30314fa3978c96aeae56c24f4ddc59b587214583
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-05 06:37:47 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_imagenet128.sh
+++ tensorflow_gan/examples/tpu/eval_imagenet128.sh
@@ -6,7 +6,7 @@ export DATASET_ARGS='--image_size=128 \
 --dataset_name=imagenet2012 \
 --num_classes=1000 \
 --dataset_val_split_name=validation'
-export ADDITIONAL='--critic_type=acgan_multiproj \
+export ADDITIONAL='--critic_type=acgan \
 --num_eval_steps=49 \
 --mode=continuous_eval \
 --extra_eval_metrics'
