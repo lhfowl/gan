78a4911e6f2f26651ecd0e7e5362a69b7073b80e
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-05 05:58:04 -0400

    WIP

--- tensorflow_gan/examples/tpu/cifar_acbaseline.sh
+++ tensorflow_gan/examples/tpu/cifar_acbaseline.sh
@@ -2,7 +2,7 @@
 # export BATCH_SIZE=64
 # export ADDITIONAL='--critic_type=kplusone_fm --kplusone_mhinge_cond_discriminator_weight=1.0 --aux_mhinge_cond_generator_weight=0.05'
 # export ADDITIONAL='--critic_type=kplusone_fm --generator_loss_fn=kplusone_ssl_featurematching_generator_loss --kplusone_mhinge_ssl_cond_discriminator_weight=1.0 --aux_mhinge_cond_generator_weight=0.05'
-export EXPERIMENT_NAME=late_cifar_base
+export EXPERIMENT_NAME=late_cifar_acbase
 export BATCH_SIZE=64
 export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=32 --dataset_name=cifar10 --num_classes=10 --dataset_val_split_name=test'
@@ -14,8 +14,10 @@ export DATASET_ARGS='--image_size=32 --dataset_name=cifar10 --num_classes=10 --d
 # --aux_mhinge_cond_generator_weight=0.05 \
 # --tpu_gan_estimator_d_step=4'
 
-
-export ADDITIONAL='--critic_type=acgan_multiproj \
+export ADDITIONAL='--critic_type=acgan \
+--aux_cond_generator_weight=0.1 \
+--aux_cond_discriminator_weight=1.0 \
+--tpu_gan_estimator_d_step=2 \
 --extra_eval_metrics \
 --eval_batch_size=1024 \
 --num_eval_steps=9'
