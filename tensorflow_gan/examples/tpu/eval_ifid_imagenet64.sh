e9713575b212021d8ecfd441c761fce0193de70e
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-10 10:30:21 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_ifid_imagenet64.sh
+++ tensorflow_gan/examples/tpu/eval_ifid_imagenet64.sh
@@ -1,13 +1,13 @@
 
-export EXPERIMENT_NAME=imagenet128_expt_3ctd_bkp795
+export EXPERIMENT_NAME=imagenet64_fixed
 export BATCH_SIZE=1024
 export TRAIN_STEPS_PER_EVAL=10000
-export DATASET_ARGS='--image_size=128 \
---dataset_name=imagenet2012 \
+export DATASET_ARGS='--image_size=64 \
+--dataset_name=imagenet_resized/64x64 \
 --num_classes=1000 \
 --dataset_val_split_name=train'
 export ADDITIONAL="--critic_type=acgan \
---num_eval_steps=100 \
+--num_eval_steps=40 \
 --mode=intra_fid_eval \
 --intra_fid_eval_chunk_size=20"
 
---
+++ tensorflow_gan/examples/tpu/eval_ifid_imagenet64_baseline.sh
@@ -0,0 +1,15 @@
+
+export EXPERIMENT_NAME=imagenet64_baseline
+export BATCH_SIZE=1024
+export TRAIN_STEPS_PER_EVAL=10000
+export DATASET_ARGS='--image_size=64 \
+--dataset_name=imagenet_resized/64x64 \
+--num_classes=1000 \
+--dataset_val_split_name=train'
+export ADDITIONAL="--critic_type=acgan \
+--num_eval_steps=40 \
+--mode=intra_fid_eval \
+--intra_fid_eval_chunk_size=20"
+
+bash tpu/_eval_base.sh
+
