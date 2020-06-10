a890f9c0d546ad2b48f55f749c97958e599a0f3c
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-10 13:41:12 -0400

    WIP

---
+++ tensorflow_gan/examples/tpu/ssl_imagenet128.sh
@@ -0,0 +1,25 @@
+
+# export BATCH_SIZE=64
+# export ADDITIONAL='--critic_type=kplusone_fm --kplusone_mhinge_cond_discriminator_weight=1.0 --aux_mhinge_cond_generator_weight=0.05'
+# export ADDITIONAL='--critic_type=kplusone_fm --generator_loss_fn=kplusone_ssl_featurematching_generator_loss --kplusone_mhinge_ssl_cond_discriminator_weight=1.0 --aux_mhinge_cond_generator_weight=0.05'
+
+export EXPERIMENT_NAME=ssl_imagenet128_withproj
+export BATCH_SIZE=1024
+export TRAIN_STEPS_PER_EVAL=10000
+export DATASET_ARGS='--image_size=128 \
+--dataset_name=imagenet2012_subset/10pct \
+--num_classes=1000 \
+--dataset_val_split_name=validation \
+--unlabelled_dataset_name=imagenet2012 \
+--unlabelled_dataset_split_name=train'
+# export ADDITIONAL='--critic_type=kplusone_fm \
+# --generator_loss_fn=kplusone_ssl_featurematching_generator_loss \
+# --kplusone_mhinge_ssl_cond_discriminator_weight=1.0 \
+# --aux_mhinge_cond_generator_weight=0.05 \
+# --tpu_gan_estimator_d_step=4'
+
+export ADDITIONAL='--critic_type=acgan \
+--aux_mhinge_cond_generator_weight=0.1 \
+--aux_mhinge_cond_discriminator_weight=1.0'
+
+bash tpu/_base.sh
