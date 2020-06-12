1a8c436bb54e4ca7016f3513b2ffa6e53d9bb27f
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-11 21:49:26 -0400

    WIP

--- tensorflow_gan/examples/tpu/imagenet128_ctdas_multiproj.sh
+++ tensorflow_gan/examples/tpu/imagenet128_ctdas_multiproj.sh
@@ -2,7 +2,7 @@
 export EXPERIMENT_NAME=imagenet128_baseline_ctd2
 export BATCH_SIZE=1024
 export TRAIN_STEPS_PER_EVAL=500
-export DATASET_ARGS='--image_size=128 --dataset_name=imagenet2012 --num_classes=1000 --dataset_val_split_name=validation'
+export DATASET_ARGS='--tpu_iterations_per_loop=500 --image_size=128 --dataset_name=imagenet2012 --num_classes=1000 --dataset_val_split_name=validation'
 
 export ADDITIONAL='--critic_type=acgan_multiproj \
 --aux_mhinge_cond_generator_weight=0.1 \
