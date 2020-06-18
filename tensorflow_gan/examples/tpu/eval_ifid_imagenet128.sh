5aff37cadbbe71e46c9d73c409994ff9997fa771
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-18 09:47:56 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_ifid_imagenet128.sh
+++ tensorflow_gan/examples/tpu/eval_ifid_imagenet128.sh
@@ -1,5 +1,5 @@
 
-export EXPERIMENT_NAME=imagenet128_expt_3ctd_bkp795
+export EXPERIMENT_NAME=imagenet128_expt_trial2_bkp
 export BATCH_SIZE=1024
 export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=128 \
