b61b0f8e48da358d98b26614285f63b901de9a81
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-18 23:43:33 -0400

    WIP

--- tensorflow_gan/examples/tpu/stl128.sh
+++ tensorflow_gan/examples/tpu/stl128.sh
@@ -3,19 +3,22 @@
 # export ADDITIONAL='--critic_type=kplusone_fm --kplusone_mhinge_cond_discriminator_weight=1.0 --aux_mhinge_cond_generator_weight=0.05'
 # export ADDITIONAL='--critic_type=kplusone_fm --generator_loss_fn=kplusone_ssl_featurematching_generator_loss --kplusone_mhinge_ssl_cond_discriminator_weight=1.0 --aux_mhinge_cond_generator_weight=0.05'
 
-export EXPERIMENT_NAME=stl128
+export EXPERIMENT_NAME=stl128_withproj
 export BATCH_SIZE=1024
-export TRAIN_STEPS_PER_EVAL=2000
+export TRAIN_STEPS_PER_EVAL=5000
 export DATASET_ARGS='--image_size=128 \
 --dataset_name=stl10 \
 --num_classes=10 \
 --dataset_val_split_name=test \
 --unlabelled_dataset_name=stl10 \
 --unlabelled_dataset_split_name=unlabelled'
-export ADDITIONAL='--critic_type=kplusone_fm \
---generator_loss_fn=kplusone_ssl_featurematching_generator_loss \
---kplusone_mhinge_ssl_cond_discriminator_weight=1.0 \
---aux_mhinge_cond_generator_weight=0.05 \
---tpu_gan_estimator_d_step=4'
+# export ADDITIONAL='--critic_type=kplusone_fm \
+# --generator_loss_fn=kplusone_ssl_featurematching_generator_loss \
+# --kplusone_mhinge_ssl_cond_discriminator_weight=1.0 \
+# --aux_mhinge_cond_generator_weight=0.05 \
+# --tpu_gan_estimator_d_step=4'
+export ADDITIONAL='--critic_type=acgan \
+--aux_mhinge_cond_generator_weight=0.1 \
+--aux_mhinge_cond_discriminator_weight=1.0'
 
 bash tpu/_base.sh
