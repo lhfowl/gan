472e83a77f596132fd25434e4239f849f3781b9a
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-24 11:02:23 -0400

    WIP

--- tensorflow_gan/examples/gpu/eval_imagenet64.sh
+++ tensorflow_gan/examples/gpu/eval_imagenet64.sh
@@ -1,21 +1,22 @@
 # export EXPERIMENT_NAME='imagenet64_baseline'
-export EXPERIMENT_NAME='imagenet64_fixed'
+# export EXPERIMENT_NAME='imagenet64_fixed'
 # export EXPERIMENT_NAME=imagenet64_acbaseline_2step
+export EXPERIMENT_NAME=imagenet64_baseline_ctd
 export OUTPUT_DIR=/scratch0/ilya/locDoc/tfgan/${EXPERIMENT_NAME}
 export DATA_DIR=/scratch1/ilya/locDoc/data/tfdf
 
 export BATCH_SIZE=128
 export DATASET_ARGS='--image_size=64 --dataset_name=imagenet_resized/64x64 --num_classes=1000 --dataset_val_split_name=validation'
 
-export ADDITIONAL='--critic_type=acgan \
---num_eval_steps=390 \
---mode=continuous_eval \
---extra_eval_metrics'
-
-# USE ONLY WITH REGULAR BASELINE!
-# export ADDITIONAL='--critic_type=acgan_multiproj \
+# export ADDITIONAL='--critic_type=acgan \
 # --num_eval_steps=390 \
 # --mode=continuous_eval \
 # --extra_eval_metrics'
+
+# USE ONLY WITH REGULAR BASELINE!
+export ADDITIONAL='--critic_type=acgan_multiproj \
+--num_eval_steps=390 \
+--mode=continuous_eval \
+--extra_eval_metrics'
 
 bash gpu/_eval_base.sh
