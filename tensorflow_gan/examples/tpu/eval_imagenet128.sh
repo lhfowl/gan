d010f1fbcfca0a01dcbd1121afdc57574ed20725
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-24 13:31:02 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_imagenet128.sh
+++ tensorflow_gan/examples/tpu/eval_imagenet128.sh
@@ -7,16 +7,16 @@ export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=128 \
 --dataset_name=imagenet2012 \
 --num_classes=1000 \
---dataset_val_split_name=validation'
-# export ADDITIONAL='--critic_type=acgan \
-# --num_eval_steps=49 \
-# --mode=continuous_eval \
-# --extra_eval_metrics'
-
-export ADDITIONAL='--critic_type=acgan_multiproj \
+--dataset_val_split_name=train'
+export ADDITIONAL='--critic_type=acgan \
 --num_eval_steps=49 \
 --mode=continuous_eval \
 --extra_eval_metrics'
+
+# export ADDITIONAL='--critic_type=acgan_multiproj \
+# --num_eval_steps=49 \
+# --mode=continuous_eval \
+# --extra_eval_metrics'
 
 bash tpu/_eval_base.sh
 
