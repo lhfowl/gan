7ccc39fcdbec129cc2a690f171610f9e442acd01
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-26 19:26:20 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_ifid_cifar100.sh
+++ tensorflow_gan/examples/tpu/eval_ifid_cifar100.sh
@@ -6,7 +6,7 @@ export DATASET_ARGS='--image_size=32 \
 --dataset_name=cifar100 \
 --num_classes=100 \
 --dataset_val_split_name=train'
-export ADDITIONAL="--critic_type=acgan \
+export ADDITIONAL="--critic_type=kplusone_fm \
 --num_eval_steps=100 \
 --mode=intra_fid_eval \
 --intra_fid_eval_chunk_size=50"
