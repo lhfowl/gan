9c55709a3a4df1e014257d2f95b466f766f7a86b
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-26 19:32:37 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_ifid_cifar100.sh
+++ tensorflow_gan/examples/tpu/eval_ifid_cifar100.sh
@@ -6,7 +6,7 @@ export DATASET_ARGS='--image_size=32 \
 --dataset_name=cifar100 \
 --num_classes=100 \
 --dataset_val_split_name=train'
-export ADDITIONAL="--critic_type=kplusone_fm \
+export ADDITIONAL="--critic_type=kplusone_wgan \
 --generator_loss_fn=kplusone_featurematching_generator_loss \
 --num_eval_steps=100 \
 --mode=intra_fid_eval \
