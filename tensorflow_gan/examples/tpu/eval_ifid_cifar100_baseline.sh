d5a870d6921e52166342cb10390774120d490059
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-26 19:47:36 -0400

    WIP

--- tensorflow_gan/examples/tpu/eval_ifid_cifar100_baseline.sh
+++ tensorflow_gan/examples/tpu/eval_ifid_cifar100_baseline.sh
@@ -1,12 +1,12 @@
 
-export EXPERIMENT_NAME=cirfar100_kp1
+export EXPERIMENT_NAME=cifar100_baseline
 export BATCH_SIZE=1024
 export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=32 \
 --dataset_name=cifar100 \
 --num_classes=100 \
 --dataset_val_split_name=train'
-export ADDITIONAL="--critic_type=kplusone_wgan \
+export ADDITIONAL="--critic_type=acgan \
 --num_eval_steps=100 \
 --mode=intra_fid_eval \
 --intra_fid_eval_chunk_size=50"
