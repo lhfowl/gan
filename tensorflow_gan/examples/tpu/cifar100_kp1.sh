a59016a4cb748d54d57331a74f3a72ecf760f164
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-26 20:34:54 -0400

    WIP

--- tensorflow_gan/examples/tpu/cifar100_kp1.sh
+++ tensorflow_gan/examples/tpu/cifar100_kp1.sh
@@ -1,7 +1,7 @@
 
-export EXPERIMENT_NAME=cirfar100_kp1
+export EXPERIMENT_NAME=cirfar100_kp1_moresave
 export BATCH_SIZE=64
-export TRAIN_STEPS_PER_EVAL=10000
+export TRAIN_STEPS_PER_EVAL=2500
 export DATASET_ARGS='--image_size=32 --dataset_name=cifar100 --num_classes=100 --dataset_val_split_name=test'
 
 # for k+1 mhinge with fm
