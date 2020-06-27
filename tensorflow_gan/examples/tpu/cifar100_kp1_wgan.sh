4b35a97fc8c06f462ff8e3114e8c3314d33f8e8b
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-06-26 20:56:45 -0400

    WIP

--- tensorflow_gan/examples/tpu/cifar100_kp1_wgan.sh
+++ tensorflow_gan/examples/tpu/cifar100_kp1_wgan.sh
@@ -1,11 +1,11 @@
 
 export EXPERIMENT_NAME=cirfar100_kp1_wgan
 export BATCH_SIZE=64
-export TRAIN_STEPS_PER_EVAL=2500
+export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=32 --dataset_name=cifar100 --num_classes=100 --dataset_val_split_name=test'
 
 # for k+1 mhinge with fm
-export ADDITIONAL='--critic_type=discriminator_32_kplusone_wgan \
+export ADDITIONAL='--critic_type=kplusone_wgan \
 --generator_loss_fn=kplusone_wasserstein_generator_loss \
 --kplusone_mhinge_cond_discriminator_weight=1.0 \
 --aux_mhinge_cond_generator_weight=0.05 \
