cc9745d9ae8c7dbb117baa3bb437fd29e1fedb8a
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-07-01 10:56:22 -0400

    WIP biggan

--- tensorflow_gan/examples/tpu/biggan128_baseline.sh
+++ tensorflow_gan/examples/tpu/biggan128_baseline.sh
@@ -4,7 +4,7 @@ export BATCH_SIZE=2048
 export TRAIN_STEPS_PER_EVAL=2500
 export DATASET_ARGS='--image_size=128 --dataset_name=imagenet2012 --num_classes=1000 --dataset_val_split_name=validation'
 
-export ADDITIONAL='--critic_type=acgan_biggan \
+export ADDITIONAL='--critic_type=biggan_acgan \
 --z_dim=120 \
 --tpu_gan_estimator_d_step=2 \
 --discriminator_lr=0.0005 \
