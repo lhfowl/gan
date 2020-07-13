fd16e765e5f80be1fe2b044886deb99bf8b918e6
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-07-13 14:02:23 -0400

    WIP

--- tensorflow_gan/examples/tpu/biggan128_96compare.sh
+++ tensorflow_gan/examples/tpu/biggan128_96compare.sh
@@ -1,7 +1,7 @@
 
 export EXPERIMENT_NAME=imagenet128_biggan_baseline96
 export BATCH_SIZE=1024
-export TRAIN_STEPS_PER_EVAL=2500
+export TRAIN_STEPS_PER_EVAL=5000
 export DATASET_ARGS='--image_size=128 --dataset_name=imagenet2012 --num_classes=1000 --dataset_val_split_name=validation'
 
 export ADDITIONAL='--critic_type=acgan \
@@ -10,6 +10,7 @@ export ADDITIONAL='--critic_type=acgan \
 --discriminator_lr=0.0005 \
 --tpu_iterations_per_loop=500 \
 --gf_dim=96 \
---df_dim=96'
+--df_dim=96 \
+--keep_checkpoint_max=12'
 
 bash tpu/_base.sh
