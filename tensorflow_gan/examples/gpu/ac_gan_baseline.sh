3a320199df2290322853550a7b8dd6f0fa57ccb6
Liam Fowl <lhfowl@gmail.com>
2020-08-28 11:15:44 -0400

    minor

--- tensorflow_gan/examples/gpu/ac_gan_baseline.sh
+++ tensorflow_gan/examples/gpu/ac_gan_baseline.sh
@@ -2,15 +2,15 @@
 # WRONG DATASET just for compatibility testing purposes
 
 export EXPERIMENT_NAME='imagenet128_acbaseline'
-export OUTPUT_DIR=/scratch0/ilya/locDoc/tfgan/examples/${EXPERIMENT_NAME}
-export DATA_DIR=/scratch1/ilya/locDoc/data/tfdf
+export OUTPUT_DIR=/home/lfowl/Desktop/SAGAN/${EXPERIMENT_NAME}
+export DATA_DIR=/home/lfowl/data
 
 export BATCH_SIZE=16
 export DATASET_ARGS='--image_size=128 --dataset_name=imagenet2012 --num_classes=1000 --dataset_val_split_name=validation'
 
 
 export ADDITIONAL='--critic_type=acgan \
---num_eval_steps=2 \
---mode=continuous_eval'
+--mode=gen_images \
+--n_images_per_side_to_gen_per_tile=1'
 
 bash gpu/_eval_base.sh
